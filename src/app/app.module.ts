efaa00f6b1518d3a2173d33435b0a985d4560bcc
arnoramon <ramonarno@hotmail.com>
2018-03-06 09:36:39 +0100

    firebase_config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,21 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth, AngularFireAuthModule } from 'angularfire2/auth';
+import { UserServiceProvider } from '../providers/user-service/user-service';
+
+
+var config = {
+  apiKey: "AIzaSyCG5IV5tEE66gr2nq5WalM9pD7_g7Nfcb0",
+  authDomain: "les08-f5c11.firebaseapp.com",
+  databaseURL: "https://les08-f5c11.firebaseio.com",
+  projectId: "les08-f5c11",
+  storageBucket: "",
+  messagingSenderId: "277673071113"
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,7 +36,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule,
+    AngularFireDatabaseModule, 
+    AngularFireAuthModule, 
+    AngularFireModule.initializeApp(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,7 +53,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UserServiceProvider
   ]
 })
 export class AppModule {}
